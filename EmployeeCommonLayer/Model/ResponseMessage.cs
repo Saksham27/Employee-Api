e18db0d51637deb6dac88e0f204e9929bab70c3b
Saksham27 <geekysaksham27@gmail.com>
2020-05-19 13:56:56 +0530

    Added : Poco class for Response message

---
+++ EmployeeCommonLayer/Model/ResponseMessage.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+namespace EmployeeCommonLayer.Model
+{
+
+    [DataContract]
+    public class ResponseMessage
+    {
+        [DataMember(Name = "Status")]
+        public bool Status { get; set; }
+
+        [DataMember(Name = "Message")]
+        public string Message { get; set; }
+
+        [DataMember(Name = "Data")]
+        public List<EmployeeModel> Data { get; set; } = null;
+    }
+}
+
